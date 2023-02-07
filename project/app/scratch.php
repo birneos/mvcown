1730d99f3ccbdab0327b03898cd3a750a23434b1
64082 <marco.linke@s-markt-mehrwert.de>
2023-02-07 10:02:18 +0100

    remove app without commits

--- project/app/scratch.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Pro Action eine eigene Request Klasse + eine Response Klasse
-// Innerhalb zwischen Request und Response würde dann die Logik umgesetzt werden
-
-// Refactoring
-// Hydrator auf die höhere Ebene packen - erledigt
-// Components Ordner für Element - erledigt
